91186f37731a6eab32609a0cfc18b2aca8e9065c
Andrey <113495541+Andreydev1@users.noreply.github.com>
2025-02-05 12:32:30 +0700

    Update MachineModel.ts

--- code/client/src/game/machine/model/MachineModel.ts
+++ code/client/src/game/machine/model/MachineModel.ts
@@ -1,29 +1,47 @@
-// import ISpriteConfig from "./interface/IDarkScreenlConfig";
+export default class MachineModel {
+  _landscapeTexture: string;
+  _portraitTexture: string;
+  _isRestarted: boolean;
+  _areMatchesPlaying: boolean;
+  private _spinCount: number; // Добавляем счетчик спинов
 
-export default class MachineModel  {
-  _landscapeTexture: string
-  _portraitTexture: string
-  _isRestarted: boolean
-  _areMatchesPlaying: boolean
   constructor() {
-    // this.config = config
-    this._isRestarted = true
-    this._areMatchesPlaying = false
+    this._isRestarted = true;
+    this._areMatchesPlaying = false;
+    this._spinCount = 0; // Инициализируем счетчик
   }
 
-  public get isRestarted(){
-    return this._isRestarted 
+  public get isRestarted() {
+    return this._isRestarted;
   }
 
-  public set isRestarted(value: boolean){
-     this._isRestarted = value
+  public set isRestarted(value: boolean) {
+    this._isRestarted = value;
   }
 
-  public get areMatchesPlaying(){
-    return this._areMatchesPlaying 
+  public get areMatchesPlaying() {
+    return this._areMatchesPlaying;
   }
 
-  public set areMatchesPlaying(value: boolean){
-     this._areMatchesPlaying = value
+  public set areMatchesPlaying(value: boolean) {
+    this._areMatchesPlaying = value;
+  }
+
+  // Добавляем методы для управления счетчиком спинов
+  public incrementSpinCount(): void {
+    this._spinCount++;
+  }
+
+  public getSpinCount(): number {
+    return this._spinCount;
+  }
+
+  public resetSpinCount(): void {
+    this._spinCount = 0;
+  }
+
+  // Метод для проверки, является ли текущий спин победным
+  public isWinSpin(): boolean {
+    return this._spinCount % 5 === 0 && this._spinCount !== 0;
   }
 }
